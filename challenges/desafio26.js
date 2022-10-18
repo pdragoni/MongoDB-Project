b2afae6d511e93ff6d174126ff235aa32366059f
pdragoni <paulohbfdf@gmail.com>
2022-10-17 23:53:44 -0300

    Req 26: Cria nova tag para elementos específicos

---
+++ challenges/desafio26.js
@@ -0,0 +1,25 @@
+db.produtos.updateMany({
+  valoresNutricionais: {
+    $elemMatch: {
+      tipo: "sódio",
+      $and: [
+        { percentual: { $gt: 20, $lt: 40 } }],
+      },
+    },
+  },
+  {
+    $push:
+    {
+      tags:
+      {
+        $each: ["contém sódio"],
+      },
+    },
+  });
+
+db.produtos.find({},
+  {
+    _id: 0,
+    nome: 1,
+    tags: 1,
+  });
